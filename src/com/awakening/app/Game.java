74051b9b27538deab93add18e42dc0098425e243
Marlena Brammer <marlena.brammer@gmail.com>
2022-12-02 18:19:59 -0500

    added game class, AB#80

--- src/com/awakening/app/Game.java
+++ src/com/awakening/app/Game.java
@@ -5,7 +5,38 @@ import java.util.Scanner;
 //Class that will control gameplay
 public class Game {
 
-    private Game(){
+    private static final Prompter prompter = new Prompter(new Scanner(System.in));
+
+    public Game(){
     }
-    
+
+    public void initGame(){
+
+            String level = "1";
+            boolean quit = false;
+            while (!quit) {
+                String selectLevel = prompter.prompt("Do you want to play Awakening? [Y/N]");
+                System.out.println();
+                switch (selectLevel) {
+                    case ("Y"):
+                        startGame();
+                        quit = true;
+                        break;
+                    case ("N"):
+                        quitGame();
+                        quit = true;
+                        break;
+                    default:
+                        System.out.println("Invalid input, please provide [Y] for Yes, [N] for No.");
+                        System.out.println();
+                }
+            }
+        }
+
+    private void quitGame() {
+    }
+
+    private void startGame() {
+    }
 }
+
