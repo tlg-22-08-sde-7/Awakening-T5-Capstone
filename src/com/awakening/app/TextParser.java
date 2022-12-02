1fd9e362a508bea8cbbfaac8c0d2f71f4494b6f4
kevinsanroman84 <kevinsanroman84@gmail.com>
2022-12-02 15:09:22 -0800

    added method to take input, AB#83

--- src/com/awakening/app/TextParser.java
+++ src/com/awakening/app/TextParser.java
@@ -1,4 +1,10 @@
 package com.awakening.app;
 
 public class TextParser {
+
+    public String[] parseInput(String input) {
+        String[] result = input.split(" ");
+
+        return result;
+    }
 }
