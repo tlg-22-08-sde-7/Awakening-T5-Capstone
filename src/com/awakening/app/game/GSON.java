81fd301821f41ef3eed6fa2b71c4c138af8c17b8
Marlena Brammer <marlena.brammer@gmail.com>
2022-12-06 16:03:59 -0500

    updating latest gson, list of rooms working, AB#92

--- src/com/awakening/app/game/GSON.java
+++ src/com/awakening/app/game/GSON.java
@@ -7,6 +7,7 @@ package com.awakening.app.game;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
 import java.util.*;
@@ -18,55 +19,18 @@ public class GSON {
     public static void main(String[] args)
     {
 
-        // Creating an object of Gson class
-        Gson gson = new Gson();
-
-        //Create a location using JSON
-        Gson roomGson = new GsonBuilder().setPrettyPrinting().create();
-        Room rm = createRoomObject();
-
-
-
-        String json = gson.toJson(rm);
-
-        //create the json output file
-        try (FileWriter writer = new FileWriter("resources/JSON/rooms.json")) {
-            gson.toJson(rm, writer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         //create an array of JSON objects
-
-        List<Room> rooms = new ArrayList<>();
-
         //read JSON from a file
-        try(Reader reader = new FileReader("resources/JSON/rooms.json")){
+        try(Reader reader = new FileReader("resources/JSON/roomsList.json")){
 
-            Room room = gson.fromJson(reader, Room.class);
-//          for(Room room : rm2) {
-//              System.out.println(room.getName());
-//              System.out.println(room.getDescription());
-//          }
+            ArrayList<Room> roomList=  new Gson().fromJson(reader, new TypeToken<List<Room>>() {}.getType());
+            roomList.forEach(System.out::println);
 
-            System.out.println(room);
         } catch (IOException e){
             e.printStackTrace();
         }
     }
 
-    //Create the room object
-    private static Room createRoomObject() {
-        Room room = new Room();
-        room.setName("Basement");
-        room.setDescription("Place where the player spawns");
-        room.setItems(Arrays.asList("Camera","Cellphone"));
-        HashMap<String, String> directions = new HashMap() {{
-            put("South","Morgue");
-        }};
-        room.setDirections(directions);
-        return room;
-    }
 }
 
 
