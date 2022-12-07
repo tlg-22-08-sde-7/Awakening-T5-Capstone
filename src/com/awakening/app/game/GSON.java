bc806b4bb4ecdc24c70f46594386f6fd4fd3531d
Marlena Brammer <marlena.brammer@gmail.com>
2022-12-07 18:47:51 -0500

    gson pulling from json file, AB#92

--- src/com/awakening/app/game/GSON.java
+++ src/com/awakening/app/game/GSON.java
@@ -4,9 +4,8 @@ package com.awakening.app.game;
 // Attributes and generate a GSON out of it
 
 // Importing input output classes
+
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
@@ -16,21 +15,50 @@ import java.util.*;
 public class GSON {
 
     // Main driver method
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
         //create an array of JSON objects
         //read JSON from a file
-        try(Reader reader = new FileReader("resources/JSON/roomsList.json")){
 
-            ArrayList<Room> roomList=  new Gson().fromJson(reader, new TypeToken<List<Room>>() {}.getType());
+//        //GENERATE ITEM LIST
+//        try (Reader reader = new FileReader("resources/JSON/items.json")) {
+//            Map<String, String> itemMap = null;
+//            itemMap = new Gson().fromJson(reader, Map.class);
+//            for (Map.Entry<String, String> entry : itemMap.entrySet()) {
+//                System.out.println(entry.getKey() + "=" + entry.getValue());
+//
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("\n");
+//        System.out.println("\n");
+
+        //GENERATE ROOM LIST
+        ArrayList<Room> roomList = null;
+        try (Reader reader = new FileReader("resources/JSON/roomsListOld.json")) {
+
+            roomList = new Gson().fromJson(reader, new TypeToken<List<Room>>() {
+            }.getType());
             roomList.forEach(System.out::println);
 
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("\n");
+        System.out.println("\n");
+
+        //GENERATE ROOMMAP
+        RoomMap roomMap = new RoomMap();
+        try (Reader reader = new FileReader("resources/JSON/roomsList.json")) {
+            roomMap = new Gson().fromJson(reader, RoomMap.class);
+            RoomMap.Basement basement = roomMap.getBasement();
+            System.out.println(basement.getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
-
 }
-
-
