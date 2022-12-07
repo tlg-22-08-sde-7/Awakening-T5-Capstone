8a36221c25880efe75f71438d2179689f3823f22
hencito <hencyperez@gmail.com>
2022-12-07 17:35:58 -0500

    This commit sets up the Item class for JSON serialization and deserialization. This will be used by GSON for the creation of JAVA objects.AB#95.

--- src/com/awakening/app/game/Item.java
+++ src/com/awakening/app/game/Item.java
@@ -1,4 +1,46 @@
 package com.awakening.app.game;
 
 public class Item {
+    private String name;
+    private String description;
+    // the charge variable will hold the charge of the camera item
+    private int charge;
+
+
+    public Item(){}
+
+    public Item(String name, String description){
+        this.name = name;
+        this.description=description;
+    }
+
+    public Item(String name, String description, int charge){
+        this.name = name;
+        this.description = description;
+        this.charge = charge;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCharge() {
+        return charge;
+    }
+
+    public void setCharge(int charge) {
+        this.charge = charge;
+    }
 }
